8819060f95cff38995c7da347c8486659e695a7c
Chevdor <chevdor@gmail.com>
2023-08-30 17:19:54 +0200

    Let the user provide an input !

--- mdlint-cli/src/main.rs
+++ mdlint-cli/src/main.rs
@@ -1,10 +1,22 @@
 use mdlint::{all, process, RuleResult};
 
 fn main() {
-    let file = "mdlint/fixtures/md014/md014_ko.md";
-    let rules = Some(all());
-    let result = process(file, rules);
-    print(result);
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.iter().len() <= 1 {
+        eprintln!("You need to pass args");
+        std::process::exit(1)
+    }
+
+    if let Some(file) = args.get(1) {
+        let rules = Some(all());
+        let result = process(file, rules);
+        print(result);
+    } else {
+        eprintln!("Provide an input file.");
+        eprintln!("For instance: ./mdlint/fixtures/md014/md014_ko.md");
+        std::process::exit(1)
+    }
 }
 
 fn print(result: Vec<RuleResult>) {
